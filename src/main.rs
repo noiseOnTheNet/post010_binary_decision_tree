748b942be0fa789fd37d351a0edb3133cfa5045d
Marco Paolo Valerio Vezzoli <marco.p.v.vezzoli@gmail.com>
2024-04-23 08:02:07 +0200

    gini index and majority prediction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use std::hash::Hash;
+use std::cmp::{Eq, PartialEq};
+use std::mem;
 use polars::prelude::*;
 mod btree;
-use std::marker::PhantomData;
+use std::{marker::PhantomData, collections::{HashSet, HashMap}};
 
 fn main() {
     let df: DataFrame = df!(
@@ -20,10 +23,14 @@ fn main() {
     println!("Hello, world!");
 }
 
-struct Decision<'a, T> {
+struct Rule<'a>{
     dimension : & 'a str,
     cutoff : f64,
     metric : f64,
+}
+
+struct Decision<'a, T> {
+    rule : Option<Rule<'a>>,
     confidence : f64,
     prediction : T
 }
@@ -35,10 +42,52 @@ struct DTreeBuilder<T>{
 }
 
 impl<T> DTreeBuilder<T>{
-    fn build_node<'a>(& self, data : DataFrame, level : usize) -> btree::Node<Decision<'a, T>>{
+    fn build_node<'a>(& self, data : DataFrame, level : usize, features: HashSet<& str>, target: & str) -> btree::Node<Decision<'a, T>>{
         todo!("implement me!")
     }
-    fn build<'a>(& self, data : DataFrame) -> btree::Tree<Decision<'a, T>>{
-        btree::Tree::from_node(self.build_node(data, 1))
+    fn build<'a>(& self, data : DataFrame, features: HashSet<& str>, target: & str) -> btree::Tree<Decision<'a, T>>{
+        btree::Tree::from_node(self.build_node(data, 1, features, target))
+    }
+}
+
+fn gini(counts : & [u64]) -> f64{
+    let sum : u64 = counts.iter().sum();
+    let result : f64 = 1.0 - counts
+        .iter()
+        .map(|c| {((*c as f64)/(sum as f64)).powi(2)})
+        .sum::<f64>();
+    result
+}
+
+fn predict_majority<'a, T : Hash + Eq + PartialEq + Copy>(values : &[T]) -> Option<Decision<'a, T>>{
+    let summary : HashMap<T, u64>= values
+        .iter()
+        .fold(
+            HashMap::new(),
+            |mut result, value| {
+                result.insert(*value,result.get(value).unwrap_or(&0)+1);
+                result
+            }
+        );
+    let (prediction, count) = summary.iter()
+        .fold(
+            (None, 0),
+            |(result, count), (key, value)|
+            if *value > count {
+                (Some(key), *value)
+            }else{
+                (result, count)
+            }
+        );
+    if let Some(result) = prediction {
+        Some(
+            Decision{
+                rule: None,
+                confidence: count as f64 / values.len() as f64,
+                prediction: *result
+            }
+        )
+    }else{
+        None
     }
 }
