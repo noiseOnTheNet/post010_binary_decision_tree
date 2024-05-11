7fa994711e5a4743da9d8d24d1f0d04b5b867c88
Marco Paolo Valerio Vezzoli <marco.p.v.vezzoli@gmail.com>
2024-05-11 23:39:41 +0200

    updated to iterate over all features

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use polars::lazy::dsl::first;
+use polars::lazy::dsl::Expr;
 use polars::prelude::*;
 use polars::series::Series;
 use std::fs::File;
@@ -16,7 +16,7 @@ fn main() -> polars::prelude::PolarsResult<()> {
         s.cast(&DataType::Categorical(None, CategoricalOrdering::Lexical))
     })?;
     println!(">>>>>>>>>>>>>>> {:?}", df);
-    let feature = "sepal_length";
+    let features = ["sepal_length", "sepal_width", "petal_length", "petal_width"];
     let target = "variety";
     let mut data = CsvReader::from_path("iris.csv")
         .unwrap()
@@ -33,12 +33,26 @@ fn main() -> polars::prelude::PolarsResult<()> {
         data.column(target)?
             .value_counts(true, true)
     );
-    let metrics = evaluate_metric(&data, feature, target);
-    println!(">>>>>>>>>>>>>>>> {:?}", metrics);
-    predict_majority_dataframe(& data, target);
-    let mut metrics =  metrics?;
+
+    let metrics: [LazyFrame; 4] = features.
+        map(|feature|
+            evaluate_metric(&data, feature, target)
+                .unwrap()
+                .lazy()
+                .with_column(feature.lit().alias("feature"))
+        );
+    let concat_rules = UnionArgs {
+        parallel: true,
+        rechunk: true,
+        to_supertypes: true,
+    };
+    let mut concat_metrics: DataFrame = concat(metrics,concat_rules)
+        .unwrap()
+        .collect()?;
+    println!("\nconcat_metrics\n{1:->0$}{2:?}{1:-<0$}\n",20,"\n",concat_metrics);
     let buffer = File::create("metrics.csv").unwrap();
-    CsvWriter::new(buffer).finish(&mut metrics).unwrap();
+    CsvWriter::new(buffer).finish(&mut concat_metrics).unwrap();
+    predict_majority_dataframe(& data, target);
     Ok(())
 }
 
@@ -54,7 +68,10 @@ fn gini(counts: &[u32]) -> f64 {
     result
 }
 
+// calculate the gini impurity metric from
+// the categorical target
 fn dataframe_gini(data: & DataFrame, target: &str) -> f64 {
+    // get the categorical target and returns it as u32 symbols
     let labels = data
         .column(target)
         .unwrap()
@@ -66,27 +83,71 @@ fn dataframe_gini(data: & DataFrame, target: &str) -> f64 {
     gini(&groups_count)
 }
 
+fn estimate_gini(data: & DataFrame, target: & str) -> f64 {
+    let label_count: DataFrame = data
+        .column(target)
+        .unwrap()
+        .categorical()
+        .unwrap()
+        .value_counts()
+        .unwrap();
+    // println!("\nlabel count\n{1:->0$}{2:?}{1:-<0$}\n",20,"\n",label_count);
+
+    let expr: Expr = (col("counts")
+        .cast(DataType::Float64)
+        / col("counts").sum())
+        .pow(2)
+        .alias("squares");
+
+    let squared: DataFrame = label_count
+        .lazy()
+        .select([expr])
+        .collect()
+        .unwrap();
+
+    // println!("\nsquared\n{1:->0$}{2:?}{1:-<0$}\n",20,"\n",squared);
+
+    let square_sum: f64 = squared
+        .column("squares")
+        .unwrap()
+        .sum()
+        .unwrap();
+
+    1.0 - square_sum
+}
+
 fn predict_majority_dataframe<'a>(data: & 'a DataFrame, target: &str) -> String{
+    // extract the categorical target column
     let labels = data
         .column(target)
         .unwrap()
         .categorical()
         .unwrap();
+
+    // count all categories and sort them
     let result_count = labels.value_counts()
         .unwrap();
     println!("{1:->0$}{2:?}{1:-<0$}",20,"\n",result_count);
+
+    // get the most frequent category
     let result_cat = result_count
         .column(target)
         .unwrap()
         .head(Some(1));
     println!("{1:->0$}{2:?}{1:-<0$}",20,"\n",result_cat);
+
+    // transform the series into a categorical vector
     let actual_cat= result_cat
         .categorical()
         .unwrap();
+
+    // collect all categories as strings
     let string_cat: Vec<Option<&str>>=actual_cat
         .iter_str()
         .collect();
     println!("{1:->0$}{2:?}{1:-<0$}",20,"\n",string_cat);
+
+    // return the most common category as a string
     return string_cat.get(0)
         .unwrap()
         .unwrap()
@@ -109,8 +170,8 @@ fn evaluate_metric(data: &DataFrame, feature: &str, target: &str) -> PolarsResul
                 let higher = data.clone().filter(&values.gt_eq(sp).ok()?).ok()?;
                 let lower = data.clone().filter(&values.lt(sp).ok()?).ok()?;
                 Some(
-                    ((higher.shape().0 as f64) * dataframe_gini(& higher, target)
-                        + (lower.shape().0 as f64) * dataframe_gini(& lower, target))
+                    ((higher.shape().0 as f64) * estimate_gini(& higher, target)
+                        + (lower.shape().0 as f64) * estimate_gini(& lower, target))
                         / (values.len() as f64),
                 )
             } else {
